c4e81d5fdf57af46d8bb614e4cf5ac81fae3bd11
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:35:28 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='NlpToolkit-SentiNet',
     version='1.0.10',
     packages=['SentiNet'],
-    url='https://github.com/olcaytaner/TurkishSentiNet-Py',
+    url='https://github.com/StarlangSoftware/TurkishSentiNet-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish SentiNet'
 )
