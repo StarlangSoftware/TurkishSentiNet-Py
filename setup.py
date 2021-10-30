792f7b8da02a5e38ac71456c6f8235b30f653b08
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:35:37 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-SentiNet',
-    version='1.0.11',
+    version='1.0.12',
     packages=['SentiNet'],
     url='https://github.com/StarlangSoftware/TurkishSentiNet-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
-    description='Turkish SentiNet'
+    description='Turkish SentiNet',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
