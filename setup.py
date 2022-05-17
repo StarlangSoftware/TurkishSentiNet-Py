56582951afd2531fe3c3a9a2d052c6d8b6b55d12
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-17 22:29:07 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='NlpToolkit-SentiNet',
-    version='1.0.16',
+    version='1.0.17',
     packages=['SentiNet', 'SentiNet.data'],
     package_data={'SentiNet.data': ['*.xml']},
     url='https://github.com/StarlangSoftware/TurkishSentiNet-Py',
