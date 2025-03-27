c6ce168156fbb7c061aeeff645fb818257541a01
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2025-03-27 23:43:22 +0300

    Updated tests.

--- test/SentiSynSetTest.py
+++ test/SentiSynSetTest.py
@@ -19,11 +19,11 @@ class SentiSynSetTest(unittest.TestCase):
         outFile.close()
         input = open("test.xml", "r")
         line = input.readline()
-        self.assertEquals("<SYNSET><ID>TUR10-1093230</ID><PSCORE>0.25</PSCORE><NSCORE>0.0</NSCORE></SYNSET>\n", line)
+        self.assertEqual("<SYNSET><ID>TUR10-1093230</ID><PSCORE>0.25</PSCORE><NSCORE>0.0</NSCORE></SYNSET>\n", line)
         line = input.readline()
-        self.assertEquals("<SYNSET><ID>TUR10-0730690</ID><PSCORE>0.0</PSCORE><NSCORE>0.0</NSCORE></SYNSET>\n", line)
+        self.assertEqual("<SYNSET><ID>TUR10-0730690</ID><PSCORE>0.0</PSCORE><NSCORE>0.0</NSCORE></SYNSET>\n", line)
         line = input.readline()
-        self.assertEquals("<SYNSET><ID>TUR10-0969360</ID><PSCORE>0.0</PSCORE><NSCORE>1.0</NSCORE></SYNSET>\n", line)
+        self.assertEqual("<SYNSET><ID>TUR10-0969360</ID><PSCORE>0.0</PSCORE><NSCORE>1.0</NSCORE></SYNSET>\n", line)
         input.close()
 
 
